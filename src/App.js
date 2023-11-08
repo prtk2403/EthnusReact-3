df6d7862a6c564938278f5fbafd48eea98cc0173
prtk2403 <prtk.2403@gmail.com>
2023-11-08 20:47:34 +0530

    Added Souce Files

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import {palette} from './components/palette';
+function App() {
+  return (
+    <div className="App">
+      <h3>Created By Pratheek Nistala <a href='https://github.com/prtk2403'>prtk2403</a></h3>
+      <palette />
+    </div>
+  );
+}
+
+export default App;
